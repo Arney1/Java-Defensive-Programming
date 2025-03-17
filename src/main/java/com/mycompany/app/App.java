e650b7e51477e0510e9329a90c6aa57233d6f7ea
Arney1 <work.henna656@passinbox.com>
2025-03-17 22:18:46 +0700

    Add user input to select test case to run

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,7 @@
 package com.mycompany.app;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  */
@@ -7,11 +9,26 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        Case1 case1 = new Case1();
-        case1.run();
-        Case2 case2 = new Case2();
-        case2.run();
-        Case3Factorial case3Factorial = new Case3Factorial();
-        case3Factorial.run();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pilih test-case (1/2/3): ");
+        int selected = scanner.nextInt();
+        switch (selected) {
+            case 1:
+                Case1 case1 = new Case1();
+                case1.run();
+                break;
+            case 2:
+                Case2 case2 = new Case2();
+                case2.run();
+                break;
+            case 3:
+                Case3Factorial case3Factorial = new Case3Factorial();
+                case3Factorial.run();
+                break;
+            default:
+                System.out.println("Input tidak valid");
+                break;
+        }
+        scanner.close();
     }
 }

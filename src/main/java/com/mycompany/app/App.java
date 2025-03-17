b64dd569b15a49cde36138d19e4c8e973967d6c1
Arney1 <work.henna656@passinbox.com>
2025-03-17 22:13:11 +0700

    Uncomment and enable Case1 and Case2 test runs

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -7,10 +7,10 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        // Case1 case1 = new Case1();
-        // case1.run();
-        // Case2 case2 = new Case2();
-        // case2.run();
+        Case1 case1 = new Case1();
+        case1.run();
+        Case2 case2 = new Case2();
+        case2.run();
         Case3Factorial case3Factorial = new Case3Factorial();
         case3Factorial.run();
     }
